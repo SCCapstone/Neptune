ed821bd3ec14a99ec4ab7ea4d1d2932bf9a5bd47
Matthew Sprinkle <sprinkm@email.sc.edu>
2022-10-23 20:48:23 -0400

    added a few class files

---
+++ Server/Classes/ClientManager.js
@@ -0,0 +1,82 @@
+class ClientManager {
+    
+    /** @typedef {Map<clientId: string, Client} */
+    #clients;
+
+    /**
+     * This is the constructor
+     */
+    constructor() {
+        this.#clients = this.#clients;
+    }
+
+    /**
+     * This will remove a Client
+     * @returns {void}
+     */
+    #removeClient() {
+        return;
+    }
+
+    /**
+     * This will add a Client
+     * @returns {void}
+     */
+    #addClient() {
+        return;
+    }
+
+    /**
+     * This will pair a new Client
+     * @param {string} name 
+     * @param {IPAddress} IPAddress 
+     * @returns {Client}
+     */
+    pair(name, IPAddress) {
+        return;
+    }
+
+    /**
+     * This will unpair the Client
+     * @param {Client} client 
+     * @returns {boolean}
+     */
+    unpair(client) {
+        return false;
+    }
+
+    /**
+     * This will return a Client
+     * @param {string} clientId 
+     * @returns {Client}
+     */
+    getClient(clientId) {
+        return;
+    }
+
+    /**
+     * This will return all the Clients
+     * @returns {Client[]}
+     */
+    getClients() {
+        return;
+    }
+
+    /**
+     * This will load all the Clients
+     * @returns {void}
+     */
+    loadClients() {
+        return;
+    }
+
+    /**
+     * This will save the current Clients
+     * @returns {void}
+     */
+    saveClients() {
+        return;
+    }
+}
+
+modules.export = ClientManager;
---
+++ Server/Classes/NeptuneNotification.js
@@ -0,0 +1,42 @@
+class NeptuneNotification {
+    
+    /** @typedef {StatusBarNotification} */
+    #Notification;
+    /** @typedef {Map<string, boolean>} */
+    #pushedClients;
+    /** @typedef {string} */
+    #id;
+    
+    /** This is the constructor */
+    constructor() {
+        this.#Notification = this.#Notification;
+        this.#pushedClients = this.#pushedClients;
+        this.#id = this.#id;
+    }
+
+    /**
+     * This will activate a notification
+     * @return {void}
+     */
+    activate() {
+        return;
+    }
+
+    /**
+     * This will dismiss the notification
+     * @return {void}
+     */
+    dismiss() {
+        return;
+    }
+
+    /**
+     * This will push the notification to the client
+     * @return {void}
+     */
+    pushToClient() {
+        return;
+    }
+}
+
+modules.export = NeptuneNotification;
