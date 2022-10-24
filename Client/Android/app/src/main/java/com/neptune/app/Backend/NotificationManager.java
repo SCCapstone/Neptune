f5327bf150c4a88914ddcc1308c762b3f12bdd61
ridgetj <ridgetj@email.sc.edu>
2022-10-23 21:35:17 -0400

    Added to Client Backend

---
+++ Client/Android/app/src/main/java/com/neptune/app/Backend/NotificationManager.java
@@ -0,0 +1,28 @@
+import android.app.Notification;
+
+import java.util.Map;
+
+public class NotificationManager extends ServerManager {
+
+    private Map<String, Notification> notifications;
+    private ServerManager serverManager;
+
+    public NotificationManager notificationManager() {
+
+        return null;
+    }
+
+    public void newNotification(Notification notification) {
+
+    }
+
+    public void updateNotification(Notification notification) {
+
+    }
+
+    public void notificationDismissed(Notification notification) {
+
+    }
+
+
+}
---
+++ Client/Android/app/src/main/java/com/neptune/app/Backend/ServerManager.java
@@ -0,0 +1,48 @@
+import java.util.Map;
+
+public class ServerManager {
+
+    private Map<serverId, Server> servers;
+
+    private void removeServer() {
+
+    }
+
+    private void addServer() {
+
+    }
+
+    public ServerManager serverManager() {
+
+        return null;
+    }
+
+    public Server pair(String name, IPAddress ipAddress) {
+
+        return null;
+    }
+
+    public boolean unpair(Server server) {
+
+        return false;
+    }
+
+    public Server getServer(serverId serverId) {
+
+        return null;
+    }
+
+    public Server[] getServers() {
+
+        return new Server[0];
+    }
+
+    public void loadServers() {
+
+    }
+
+    public void saveServers() {
+
+    }
+
+}
