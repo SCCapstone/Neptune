a5b61afc6d5213155ce6ffafa320241f13945d78
ridgetj <ridgetj@email.sc.edu>
2022-10-23 20:55:05 -0400

    Added to Client Backend

---
+++ Client/Android/app/src/main/java/com/example/neptuneclientbackend/NeptuneNotification.java
@@ -0,0 +1,25 @@
+import android.app.Notification;
+
+import java.util.Map;
+
+public class NeptuneNotification extends NotificationListenerService {
+
+    private Notification statusBarNotification;
+    private Map<String, Boolean> pushedServers;
+
+    public Notification notification() {
+        return null;
+    }
+
+    public void activate() {
+
+    }
+
+    public void dismiss() {
+
+    }
+
+    public void pushToServer() {
+
+    }
+}
