b38c7dd1e5cb0b9581237991fe880822728ece92
wmamos <wmamos@email.sc.edu>
2022-11-13 19:59:45 -0500

    Add button functionality, added new layout files, and changed main activity layout file

--- Client/Android/Readme.md
+++ Client/Android/Readme.md
@@ -3,6 +3,6 @@
 
 Client application for Android. Sends notification data (your Android device's notifications) to a remote server (the Server application).
 
-Written in Kotlin (undetermined Android API level).\
+Written in Java (undetermined Android API level).\
 More info to come. Use this as the root path for Android app development.\
 https://developer.android.com/studio
--- Client/Android/app/src/main/java/com/neptune/app/MainActivity.java
+++ Client/Android/app/src/main/java/com/neptune/app/MainActivity.java
@@ -1,11 +1,19 @@
 package com.neptune.app;
 
+//Create a method that searches the ServerManager's Map of servers and compare the serverId with the textView that we wanna delete.
+//Use either list views to make a lists of the names and edit buttons, or create a new TextView and ImageView for each new device. Depending on what you choose
+// you will have to delete the entries from the list or the entire views when removing a device.
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.DialogFragment;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.content.Intent;
 
@@ -14,6 +22,9 @@ public class MainActivity extends AppCompatActivity implements RenameDialog.Rena
     //public Config config
     private TextView devName;
     private ImageView editName;
+    private Button add;
+    private AlertDialog addDialog;
+    private LinearLayout addLine;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +32,11 @@ public class MainActivity extends AppCompatActivity implements RenameDialog.Rena
         setContentView(R.layout.activity_main);
 
         devName = (TextView) findViewById(R.id.name);
+        editName = (ImageView) findViewById(R.id.editDevName);
+        add = (Button) findViewById(R.id.addDev);
+        addLine = findViewById(R.id.container);
+        buildAddDialog();
+
         devName.setOnClickListener(new TextView.OnClickListener(){
             @Override
             public void onClick(View v) {
@@ -28,13 +44,67 @@ public class MainActivity extends AppCompatActivity implements RenameDialog.Rena
             }
         });
 
-        editName = (ImageView) findViewById(R.id.editDevName);
         editName.setOnClickListener(new ImageView.OnClickListener(){
             @Override
             public void onClick(View v) {
                 openDialog();
             }
         });
+
+        add.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                addDialog.show();
+            }
+        });
+    }
+
+    private void buildAddDialog() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        View view = getLayoutInflater().inflate(R.layout.add_dialog, null);
+
+        final EditText name = view.findViewById(R.id.nameEdit);
+
+        builder.setView(view);
+        builder.setTitle("Enter name")
+                .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        addNameLine(name.getText().toString());
+                    }
+                })
+                .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+
+                    }
+                });
+
+        addDialog = builder.create();
+    }
+
+    public void addNameLine(String name) {
+        final View view = getLayoutInflater().inflate(R.layout.name_line, null);
+
+        devName = view.findViewById(R.id.name);
+        editName = view.findViewById(R.id.editName1);
+
+        devName.setText(name);
+        devName.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, DeviceActivity.class));
+            }
+        });
+
+        editName.setOnClickListener(new ImageView.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                openDialog();
+            }
+        });
+
+        addLine.addView(view);
     }
 
     public void openDialog(){
