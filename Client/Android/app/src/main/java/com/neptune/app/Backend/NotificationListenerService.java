5fb2a47be4836cbfe7d5e9319de13a5466c4c160
ridgetj <ridgetj@email.sc.edu>
2022-11-13 20:52:48 -0500

    Update NotificationListenerService.java

--- Client/Android/app/src/main/java/com/neptune/app/Backend/NotificationListenerService.java
+++ Client/Android/app/src/main/java/com/neptune/app/Backend/NotificationListenerService.java
@@ -1,13 +1,29 @@
 package com.neptune.app.Backend;
 
 import android.app.Notification;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.IntentFilter;
 import android.os.IBinder;
 import android.content.Intent;
 import android.service.notification.StatusBarNotification;
+import android.util.Log;
 
 public class NotificationListenerService extends android.service.notification.NotificationListenerService {
 
     private NotificationManager notificationManager;
+
+    private String TAG = this.getClass().getSimpleName();
+    private NotificationServiceReceiver notificationServiceReceiver;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        notificationServiceReceiver = new NotificationServiceReceiver();
+        IntentFilter filter = new IntentFilter();
+        filter.addAction("com.neptune.app.Backend");
+        registerReceiver(notificationServiceReceiver, filter);
+    }
 
     public void cancelNotification (Notification[] notification) {
 
@@ -33,14 +49,24 @@ public class NotificationListenerService extends android.service.notification.No
 
     public void onDestroy() {
         super.onDestroy();
+        unregisterReceiver(notificationServiceReceiver);
     }
 
     public void onNotificationPosted(StatusBarNotification notification) {
-        onNotificationPosted(notification);
+        Log.i(TAG, "****** onNotificationPosted");
+        Log.i(TAG, "ID :" + notification.getId() + "\t" + notification.getNotification().tickerText + "\t" + notification.getPackageName());
+        Intent i = new Intent("com.neptune.app.Backend");
+        i.putExtra("notification_event", "onNotificationPosted :" + notification.getPackageName() + "\n");
+        sendBroadcast(i);
     }
 
     public void onNotificationRemoved(StatusBarNotification notification) {
-        onNotificationRemoved(notification);
+        Log.i(TAG, "******* onNotificationRemoved");
+        Log.i(TAG, "ID :" + notification.getId() + "\t" + notification.getNotification().tickerText + "\t" + notification.getPackageName());
+        Intent i = new Intent("com.neptune.app.Backend");
+        i.putExtra("notification_event", "onNotificationRemoved :" + notification.getPackageName() + "\n");
+        sendBroadcast(i);
+
     }
 
     public void onSilentStatusBariconsVisibilityChanged(boolean status) {
@@ -61,5 +87,34 @@ public class NotificationListenerService extends android.service.notification.No
 
     public void snoozeNotification(Notification notification) {
 
+    }
+
+    class NotificationServiceReceiver extends BroadcastReceiver {
+
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            if (intent.getStringExtra("command").equals("clearall")) {
+                NotificationListenerService.this.cancelAllNotifications();
+            }
+
+            else if (intent.getStringExtra("command").equals("list")) {
+                Intent intent1 = new Intent("com.neptune.app.Backend");
+                intent1.putExtra("notification_event", "=============");
+                sendBroadcast(intent1);
+                int i = 1;
+
+                for (StatusBarNotification notification : NotificationListenerService.this.getActiveNotifications()) {
+                    Intent intent2 = new Intent("com.neptune.app.Backend");
+                    intent2.putExtra("notification_event", i + " " + notification.getPackageName() + "\n");
+                    sendBroadcast(intent2);
+                    i++;
+                }
+
+                Intent intent3 = new Intent("com.neptune.app.Backend");
+                intent3.putExtra("notification_event", "==== Notification List ====");
+                sendBroadcast(intent3);
+            }
+
+        }
     }
 }
