9c790673aa8c2270f29597494ef92d0686625d5b
wmamos <wmamos@email.sc.edu>
2022-11-23 00:13:34 -0500

    NotificationsActivity: list off checkboxes of downloaded apps #41

--- Client/Android/app/src/main/java/com/neptune/app/NotificationsActivity.java
+++ Client/Android/app/src/main/java/com/neptune/app/NotificationsActivity.java
@@ -2,11 +2,24 @@ package com.neptune.app;
 
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.DialogFragment;
 
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
+import android.content.res.Resources;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+
+import java.util.Arrays;
+import java.util.List;
 
 public class NotificationsActivity extends AppCompatActivity {
+
+    Button showBut;
+    ListView notifView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +30,39 @@ public class NotificationsActivity extends AppCompatActivity {
         if(aBar!=null) {
             aBar.setDisplayHomeAsUpEnabled(true);
         }
+
+        notifView = findViewById(R.id.listview);
+        showBut = findViewById(R.id.check);
+        showBut.setVisibility(View.INVISIBLE);
+        showBut.performClick();
+    }
+
+    public void getallapps(View view) throws PackageManager.NameNotFoundException{
+        final Intent mainIntent = new Intent(Intent.ACTION_MAIN, null);
+        mainIntent.addCategory(Intent.CATEGORY_LAUNCHER);
+
+        List<ResolveInfo> ril = getPackageManager().queryIntentActivities(mainIntent, 0);
+        //List<String> componentList = new ArrayList<String>();
+        String name = null;
+        int i = 0;
+
+        String[] apps = new String[ril.size()];
+        for (ResolveInfo ri : ril) {
+            if (ri.activityInfo != null) {
+                Resources res = getPackageManager().getResourcesForApplication(ri.activityInfo.applicationInfo);
+                if (ri.activityInfo.labelRes != 0) {
+                    name = res.getString(ri.activityInfo.labelRes);
+                } else {
+                    name = ri.activityInfo.applicationInfo.loadLabel(
+                            getPackageManager()).toString();
+                }
+                apps[i] = name;
+                i++;
+            }
+        }
+
+        Arrays.sort(apps);
+        notifView.setAdapter(new ArrayAdapter<String>(NotificationsActivity.this, android.R.layout.simple_list_item_multiple_choice, apps));
     }
 
     @Override
