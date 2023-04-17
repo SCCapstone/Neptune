f3458aade3773343a0a31194f7e298b35f359959
wmamos <wmamos@email.sc.edu>
2023-04-16 21:22:11 -0400

    Some behavior tests for client. #134

---
+++ Client/Android/app/src/androidTest/java/com/neptune/app/EnableClipboardWidgetsTest.java
@@ -0,0 +1,54 @@
+package com.neptune.app;
+
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.isEnabled;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static org.hamcrest.Matchers.not;
+
+import androidx.test.espresso.Espresso;
+import androidx.test.ext.junit.rules.ActivityScenarioRule;
+
+import com.neptune.app.Backend.IPAddress;
+import com.neptune.app.Backend.Server;
+
+import org.junit.Rule;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.util.UUID;
+
+public class EnableClipboardWidgetsTest {
+
+
+    @Rule
+    public ActivityScenarioRule<MainActivity> mainRule = new ActivityScenarioRule<>(MainActivity.class);
+
+    @Test
+    public void checkClipBoardWidgetsDisabled() {
+        try {
+            //Creates a mock server so the client interactions can be tested.
+            Server testServer = new Server(UUID.randomUUID(), MainActivity.configurationManager);
+            testServer.friendlyName = "testServer";
+            testServer.ipAddress = new IPAddress("1.1.1.1:50000");
+            MainActivity.serverManager.addServer(testServer);
+            mainRule.getScenario().recreate();
+            Espresso.onView(withId(R.id.server_name)).perform(click());
+
+            //Making sure that the related checkbox gets checked.
+            if(!testServer.clipboardSettings.enabled) {
+                Espresso.onView(withId(R.id.clipboard_enable)).perform(click());
+            }
+
+            //Checks to see if the buttons are enabled because the checkbox is checked.
+            Espresso.onView(withId(R.id.btnSendClipboard)).check(matches(isEnabled()));
+            Espresso.onView(withId(R.id.btnReceiveClipboard)).check(matches(isEnabled()));
+            Espresso.onView(withId(R.id.clipboard_server_get)).check(matches(isEnabled()));
+
+            MainActivity.serverManager.removeServer(testServer);
+            testServer.delete();
+        }  catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ Client/Android/app/src/androidTest/java/com/neptune/app/OpenServerSettingsTest.java
@@ -0,0 +1,63 @@
+package com.neptune.app;
+
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.action.ViewActions.click;
+
+import android.content.Intent;
+
+import androidx.test.espresso.Espresso;
+import androidx.test.espresso.intent.Intents;
+import androidx.test.espresso.intent.matcher.IntentMatchers;
+import androidx.test.ext.junit.rules.ActivityScenarioRule;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+
+import com.neptune.app.Backend.IPAddress;
+import com.neptune.app.Backend.Server;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import java.io.IOException;
+import java.util.UUID;
+
+@RunWith(AndroidJUnit4.class)
+public class OpenServerSettingsTest {
+
+    @Before
+    public void setUp() {
+        Intents.init();
+    }
+
+    @Rule
+    public ActivityScenarioRule<MainActivity> mainRule = new ActivityScenarioRule<>(MainActivity.class);
+
+    @Test
+    public void openServerSettings() {
+
+        try {
+            //Creates a mock server so the client interactions can be tested.
+            Server testServer = new Server(UUID.randomUUID(), MainActivity.configurationManager);
+            testServer.friendlyName = "testServer";
+            testServer.ipAddress = new IPAddress("1.1.1.1:50000");
+            MainActivity.serverManager.addServer(testServer);
+            mainRule.getScenario().recreate();
+
+            Espresso.onView(withId(R.id.server_name)).perform(click());
+            Intents.intended(IntentMatchers.hasComponent(ServerSettingsActivity.class.getName()));
+
+            MainActivity.serverManager.removeServer(testServer);
+            testServer.delete();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @After
+    public void tearDown() {
+        Intents.release();
+    }
+
+}
